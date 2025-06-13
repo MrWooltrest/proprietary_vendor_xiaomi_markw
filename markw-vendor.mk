916b3038347fc372166db1d4171ee1795cee081a
wltrest <wooltrest@gmail.com>
2025-06-13 17:21:51 +0300

    markw: Import consumerir blob

--- markw-vendor.mk
+++ markw-vendor.mk
@@ -726,6 +726,7 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.uceservice@2.3 \
     android.hardware.bluetooth@1.0-impl-qti \
     com.dsi.ant@1.0-impl \
+    consumerir.default \
     fingerprint.goodix \
     fingerprint.searchf \
     gatekeeper.msm8953 \
